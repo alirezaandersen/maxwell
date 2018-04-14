115af1fc9845a774a9aa1339b8e94a072e1e033e
Alireza Andersen <alireza.andersen@gmail.com>
2018-04-13 18:58:51 -0700

    added output to exercise file. Will create specific file for dhh count.

--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,9 @@
-class Event
+require_relative 'event'
 
-  attr_accessor :event_type
-  attr_accessor :score
-
-  def initialize(event_type, score = 0 )
-    @event_type = event_type
-    @score = score
-  end
-end
+event1 = Event.new("IssueEvent", 7)
+event2 = Event.new("IssueCommentEvent", 6)
+event3 = Event.new("PushEvent", 5)
+event4 = Event.new("PullRequestReviewCommentEvent", 4)
+event5 = Event.new("WatchEvent", 3)
+event6 = Event.new("CreateEvent", 2)
+event7 = Event.new("AnyOtherEvent", 1)
