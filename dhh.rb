676b0b8f56573a57f4a51162cd9367f02fa75498
Alireza Andersen <alireza.andersen@gmail.com>
2018-04-15 07:14:54 -0700

    add events to hash

--- dhh.rb
+++ dhh.rb
@@ -1,4 +1,16 @@
 class DHH
 
+  attr_accessor :title
+  attr_accessor :all_events
 
-end 
+  def initialize(title)
+    @title = title
+    @all_events = Hash.new(0)
+  end
+
+  def add_events_to_hash(event)
+   @all_events[event_type.name] += event.score
+ end
+
+
+end
